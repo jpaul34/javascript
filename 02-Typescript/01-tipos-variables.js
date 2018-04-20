0447dbd24184f9c2adc835be7c23d5faa6364e83
Jonathan Caiza <jpaul.snp@gmail.com>
2018-04-20 17:11:10 -0500

    Typescript

---
+++ 02-Typescript/01-tipos-variables.js
@@ -0,0 +1,26 @@
+//var nombre = 'Jonathan'
+var segundoNombre = 'Jonathan';
+var cedula = '1234567890';
+segundoNombre = 'Paul';
+//cedula = '0123456789'   // Da error read only
+var URL_POLI = 'http://www.epn.edu.ec';
+//segundoNombre = 123;   // Duck Typing
+var nombre;
+var edad = 23;
+var fecha = new Date();
+var casado = false;
+//let mascotas:objeto = {};
+var Usuario = /** @class */ (function () {
+    function Usuario(nombre, edad, cedula) {
+        this.mNombre = nombre;
+        this.mEdad = edad;
+        this.mCedula = cedula;
+    }
+    Usuario.prototype.imprimirEnConsola = function () {
+        //Template String
+        console.log("El usuario se llama \n        " + this.mNombre + " la cedula del usuario es \n        " + this.mCedula + ", la edad del usuario es:\n        " + this.mEdad);
+    };
+    return Usuario;
+}());
+var usuario = new Usuario('jonathan', 23, '123456789');
+console.log(usuario);
---
+++ 02-Typescript/01-tipos-variables.ts
@@ -0,0 +1,46 @@
+//var nombre = 'Jonathan'
+let segundoNombre = 'Jonathan'
+const cedula = '1234567890'
+
+segundoNombre = 'Paul'
+//cedula = '0123456789'   // Da error read only
+
+const URL_POLI = 'http://www.epn.edu.ec';
+
+//segundoNombre = 123;   // Duck Typing
+
+let nombre: String;
+let edad: Number = 23;
+let fecha: Date = new Date();
+let casado: boolean = false;
+//let mascotas:objeto = {};
+
+class Usuario {
+
+    public mNombre: String;
+    private mEdad: Number;
+    protected mCedula: String;
+
+
+    constructor( nombre: String,
+                 edad: Number,
+                 cedula: String){
+        this.mNombre = nombre;
+        this.mEdad = edad;
+        this.mCedula = cedula;
+    }
+
+    public imprimirEnConsola(){
+        //Template String
+        console.log(`El usuario se llama 
+        ${this.mNombre} la cedula del usuario es 
+        ${this.mCedula}, la edad del usuario es:
+        ${this.mEdad}`);
+    }
+
+
+}
+
+let usuario: Usuario = new Usuario('jonathan', 23, '123456789');
+console.log(usuario);
+
